2db9f8ae2eced6e8c5176e9c537c9d92d29e1524
rkozhan <yalta.vrk@gmail.com>
2022-02-10 11:26:23 +0200

    fixes

--- js/menu-links.js
+++ js/menu-links.js
@@ -7,26 +7,27 @@ const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
 if (menuLinks.length > 0) {
 	menuLinks.forEach(menuLink => {
 		menuLink.addEventListener('click', onMenuLinkClick);
+
 	});
 
 	function onMenuLinkClick(e) {
 		const menuLink = e.target;
+		console.log(menuLink);
 		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
 			const gotoBlock = document.querySelector(menuLink.dataset.goto);
 			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight;
-
-			if (menuIcon.classList.contains('_active')) {
-				menuIcon.classList.remove('_active');
-				menuBody.classList.remove('_active');
-				document.body.classList.remove('_lock');
-			}
-
 
 			window.scrollTo({
 				top: gotoBlockValue,
 				behavior: 'smooth'
 			});
 			e.preventDefault();
+		}
+
+		if (menuIcon.classList.contains('_active')) {
+			menuIcon.classList.remove('_active');
+			menuBody.classList.remove('_active');
+			document.body.classList.remove('_lock');
 		}
 	}
 }
